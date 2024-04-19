70312523bf2ee7c3da772b4da7356118034095d2
Oz Fox <54425262+OzKitsune@users.noreply.github.com>
2024-04-19 18:37:22 +1000

    add Interactable.gd

---
+++ Code/Interactable.gd
@@ -0,0 +1,29 @@
+extends Node
+
+
+@export_multiline var message: String
+@export var sprite: Sprite2D
+
+var default_modulate: Color
+var player_inside: bool = false
+
+
+func _ready():
+	default_modulate = sprite.modulate
+
+
+func _input(event):
+	if player_inside and event.is_action_pressed("action"):
+		Globals.hud.show_message(message)
+
+
+func _on_area_2d_body_entered(body):
+	if body is Player:
+		player_inside = true
+		sprite.modulate = Color.BLANCHED_ALMOND 
+
+
+func _on_area_2d_body_exited(body):
+	if body is Player:
+		player_inside = false
+		sprite.modulate = default_modulate
