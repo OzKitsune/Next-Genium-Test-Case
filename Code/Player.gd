c586e6347a0e7049a6124c2db6da550e2767693b
Oz Fox <54425262+OzKitsune@users.noreply.github.com>
2024-04-19 11:53:46 +1000

    add Player

---
+++ Code/Player.gd
@@ -0,0 +1,50 @@
+extends CharacterBody2D
+
+@export_category("Параметры персонажа")
+@export_range(10, 100) var speed: int
+@export_range(10, 100) var max_hp: int = 10
+
+var hp: int = max_hp
+var keys: int = 0
+
+@export_category("Параметры персонажа")
+@export var interaction_area: Area2D
+@export var animation_tree: AnimationTree
+
+@onready var state_machine: AnimationNodeStateMachinePlayback = animation_tree["parameters/playback"]
+
+enum State {IDLE, WALK}
+var current_state: State = State.IDLE
+
+var blend_position_paths: Dictionary = {
+	State.IDLE : "parameters/idle/idle_bs2d/blend_position",
+	State.WALK : "parameters/walk/walk_bs2d/blend_position",
+}
+var anim_tree_state_keys: Dictionary = {
+	State.IDLE : "idle",
+	State.WALK : "walk",
+}
+
+
+#func _ready():
+	#pass
+
+
+func _physics_process(_delta):
+	move()
+
+
+func move() -> void:
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * speed
+	
+	if input_direction == Vector2.ZERO:
+		current_state = State.IDLE
+	else:
+		current_state = State.WALK
+		animation_tree.set(blend_position_paths[State.IDLE], input_direction)
+		animation_tree.set(blend_position_paths[State.WALK], input_direction)
+	
+	state_machine.travel(anim_tree_state_keys[current_state])
+	
+	move_and_slide()
