39058135d873eafe574264610c35178d04fd32f3
Oz Fox <54425262+OzKitsune@users.noreply.github.com>
2024-04-19 18:36:53 +1000

    add HUD

---
+++ Code/HUD.gd
@@ -0,0 +1,28 @@
+class_name HUD
+
+extends CanvasLayer
+
+
+@export var hp_text_label: RichTextLabel
+@export var keys_text_label: RichTextLabel
+
+@export var message_text_label: RichTextLabel
+@export var message_player: AnimationPlayer
+
+
+const HP_TEXT: String = "HP %s / %s"
+const KEYS_TEXT: String = "Ключи: %s"
+const MESSAGE_TEXT: String = "[center]%s"
+
+
+func set_hp(hp: int, max_hp: int) -> void:
+	hp_text_label.text = HP_TEXT % [hp, max_hp]
+
+
+func set_keys(number: int) -> void:
+	keys_text_label.text = KEYS_TEXT % number
+
+
+func show_message(text: String) -> void:
+	message_text_label.text = MESSAGE_TEXT % text
+	message_player.play("show")
