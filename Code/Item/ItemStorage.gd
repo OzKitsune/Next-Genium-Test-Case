6e4f9526d41bcaf37dcf5eea2f7238d0b3a370c3
Oz Fox <54425262+OzKitsune@users.noreply.github.com>
2024-04-20 14:10:52 +1000

    add ItemStorage.gd

---
+++ Code/Item/ItemStorage.gd
@@ -0,0 +1,38 @@
+## Класс хранилище предметов.
+##
+## Модель данных, предназначенная для хранения упорядоченного набора предметов.
+## Минимальный размер хранилища - 1.
+class_name ItemStorage
+
+extends Object
+
+
+var number_of_slots: int
+var items: Array = []
+
+
+func _init(slots: int):
+	if slots < 1:
+		slots = 1
+	
+	number_of_slots = slots
+	items.resize(slots)
+
+
+## Добавить предмет в первую свободную ячейку, если это возмозжно.
+## В случае успеха возвращает true.
+func add_item(item: ItemData) -> bool:
+	var index = items.find(null)
+	if index == -1: ## Если в массиве нет null, значит все ячейки заняты.
+		return false
+	
+	items[index] = item.item_script.new()
+	print("Добавлен новый предмет: " + str(item.title))
+	return true
+
+
+## Извлечь предмет из указанной ячейки.
+func extract_item(slot_index: int) -> Item:
+	var item =  items.pop_at(slot_index)
+	items.resize(number_of_slots)
+	return item
