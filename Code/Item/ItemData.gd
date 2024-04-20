b6e1f5b10ccb41ab6a39676b49c176d724e2ec22
Oz Fox <54425262+OzKitsune@users.noreply.github.com>
2024-04-20 14:10:38 +1000

    add ItemData.gd

---
+++ Code/Item/ItemData.gd
@@ -0,0 +1,11 @@
+class_name ItemData
+
+extends Resource
+
+@export var item_script: GDScript
+@export var title: String
+## Будет ли предмет добавляться в инвентарь.
+@export var collectable: bool = true
+## Будет ли расходоваться предмет при использовании.
+@export var consumable: bool = true
+@export var image: Texture2D
