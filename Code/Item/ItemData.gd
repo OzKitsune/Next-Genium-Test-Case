2041b8d47e3822accbb7e602680897b9a7877397
Oz Fox <54425262+OzKitsune@users.noreply.github.com>
2024-04-21 09:57:41 +1000

    update ItemData.gd
    - Добавлен метод create_item() -> Item.

--- Code/Item/ItemData.gd
+++ Code/Item/ItemData.gd
@@ -3,10 +3,16 @@ class_name ItemData
 extends Resource
 
 @export var item_script: GDScript
-@export var title: String
-@export var description: String
+@export var name: String
+@export_multiline var description: String
 ## Будет ли предмет добавляться в инвентарь.
 @export var collectable: bool = true
 ## Будет ли расходоваться предмет при использовании.
 @export var consumable: bool = true
 @export var image: Texture2D
+
+
+func create_item() -> Item:
+	var item = item_script.new() as Item
+	item.item_data = self
+	return item
