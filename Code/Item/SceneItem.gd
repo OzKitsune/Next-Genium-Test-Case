976594da6ccc32e07cb22f3f9d041c8868c17e4a
Oz Fox <54425262+OzKitsune@users.noreply.github.com>
2024-04-21 09:58:10 +1000

    add SceneItem.gd

---
+++ Code/Item/SceneItem.gd
@@ -0,0 +1,30 @@
+@tool
+class_name SceneItem
+
+extends Node2D
+
+
+var start_index: int 
+
+@export var item_data: ItemData
+
+@export var sprite: Sprite2D
+@export var area: Area2D
+
+
+func _ready():
+	sprite.texture = item_data.image
+	start_index = get_index()
+	area.body_entered.connect(collect)
+
+
+func collect(body: Node2D) -> void:
+	if body is Player:
+		var player = body as Player
+		if item_data.collectable:
+			if player.inventory.add_item(item_data):
+				queue_free()
+		else:
+			var item = item_data.create_item()
+			item.apply(player)
+			queue_free()
