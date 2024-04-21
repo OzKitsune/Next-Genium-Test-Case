c13b553a2927b279d7343ce789141df49dd0b5fa
Oz Fox <54425262+OzKitsune@users.noreply.github.com>
2024-04-21 14:31:48 +1000

    update Door.gd

--- Code/Door.gd
+++ Code/Door.gd
@@ -16,16 +16,21 @@ extends Node2D
 
 
 func _on_interaction_area_2d_body_entered(body):
-	if body is Player:
+	if body is Player and not is_closed:
 		animated_sprite.play("opening")
 
 
 func _on_interaction_area_2d_body_exited(body):
-	if body is Player:
+	if body is Player and not is_closed:
 		animated_sprite.play("closing")
 
 
 func _on_transition_area_2d_body_entered(body):
 	if body is Player:
-		if Globals.game != null:
-			Globals.game.to_location(to_scene, player_position)
+		var player = body as Player
+		if is_closed and can_opened_with_key:
+			if await player.use_key():
+				is_closed = false
+				Game.instance.to_location(to_scene, player_position)
+		else:
+			Game.instance.to_location(to_scene, player_position)
