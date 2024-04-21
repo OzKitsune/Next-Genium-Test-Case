f46aafed3bc8c4fb73a58a558fb82096b2679e77
Oz Fox <54425262+OzKitsune@users.noreply.github.com>
2024-04-21 18:38:37 +1000

    update Game.gd
    - Добавлен метод окончания игры, который срабатывает в случае смерти игрока;
    - Добавлено поле последнего положения игрока на улице.

--- Code/Game.gd
+++ Code/Game.gd
@@ -9,10 +9,12 @@ static var instance: Game
 @export var player: Player
 @export var location: Location
 
+var last_position_on_street: Vector2 = Vector2.ZERO
 
 func _ready():
 	instance = self
 	hud.inventory.item_storage_control.set_item_storage(player.inventory)
+	player.died.connect(end_game)
 
 
 func _input(event):
@@ -32,6 +34,9 @@ func to_location(path_to_scene: String, player_position: Vector2 = Vector2.ZERO)
 	if !ResourceLoader.exists(path_to_scene):
 		print("Сцена '%s' не существует." % path_to_scene)
 		return
+	
+	if location.type == location.Type.STREET:
+		last_position_on_street = player.position
 		
 	if location != null:
 		location.save_state()
@@ -40,7 +45,10 @@ func to_location(path_to_scene: String, player_position: Vector2 = Vector2.ZERO)
 	location = load(path_to_scene).instantiate()
 	world.call_deferred("add_child", location)
 	
-	player.position = player_position
+	if location.type == location.Type.STREET:
+		player.position = last_position_on_street + Vector2(0, 20)
+	else:
+		player.position = player_position
 
 
 func block_player_control(value: bool) -> void:
@@ -48,3 +56,9 @@ func block_player_control(value: bool) -> void:
 		player.current_state = player.State.BLOCK_CONTROL
 	else:
 		player.current_state = player.State.IDLE
+
+
+func end_game() -> void:
+	print("У вас закончилось здоровье! Запуск новой игры.")
+	Location.clear_states()
+	get_tree().change_scene_to_file("res://Scenes/Game.tscn")
