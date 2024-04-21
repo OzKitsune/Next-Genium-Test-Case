a67b6b1b8e490f3e60734fafc3e8f18a3b2732ae
Oz Fox <54425262+OzKitsune@users.noreply.github.com>
2024-04-21 11:34:00 +1000

    update Item.gd
    - Добавлена функция выбрасывания предмета.

--- Code/Item/Item.gd
+++ Code/Item/Item.gd
@@ -8,3 +8,10 @@ extends RefCounted
 
 func apply(_player: Player):
 	pass
+
+
+func drop(sender: Node, position: Vector2):
+	var item_scene = ResourceLoader.load(item_data.path_to_scene) as PackedScene
+	var item = item_scene.instantiate() as Node2D
+	sender.get_tree().current_scene.add_child(item)
+	item.position = position
