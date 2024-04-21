6be1bd8f9f047d94d4cbb3365ccaed5ff8fa03bd
Oz Fox <54425262+OzKitsune@users.noreply.github.com>
2024-04-21 15:50:22 +1000

    update Item.gd

--- Code/Item/Item.gd
+++ Code/Item/Item.gd
@@ -10,8 +10,8 @@ func apply(_player: Player):
 	pass
 
 
-func drop(sender: Node, position: Vector2):
+func drop(_sender: Node, position: Vector2):
 	var item_scene = ResourceLoader.load(item_data.path_to_scene) as PackedScene
 	var item = item_scene.instantiate() as Node2D
-	sender.get_tree().current_scene.add_child(item)
+	Game.instance.location.add_child(item)
 	item.position = position
