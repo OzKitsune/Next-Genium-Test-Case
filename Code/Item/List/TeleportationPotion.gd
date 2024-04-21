70306e34279b2678a3d4febfd89658433945368f
Oz Fox <54425262+OzKitsune@users.noreply.github.com>
2024-04-21 19:20:50 +1000

    update TeleportationPotion.gd
    - Добавлена реализация метода apply(player: Player).

--- Code/Item/List/TeleportationPotion.gd
+++ Code/Item/List/TeleportationPotion.gd
@@ -6,6 +6,18 @@ class_name TeleportationPotion
 
 extends Item
 
+var max_random: int = 80
+
 
 func apply(player: Player):
-	print("Зелье телепортации!")
+	randomize()
+	var random_1 = randf_range(-max_random, max_random);
+	var random_2 = randf_range(-max_random, max_random);
+	var random_position = Vector2(random_1, random_2) + player.global_position
+	var space_state = player.get_world_2d().direct_space_state
+	var query = PhysicsRayQueryParameters2D.create(player.global_position, random_position)
+	var result = space_state.intersect_ray(query)
+	if result.is_empty():
+		player.global_position = random_position
+	else:
+		player.global_position = result["position"]
